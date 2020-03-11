299e6fced67508689a9366086e4491f6ebb6d8dd
wyd90 <thuaaaaabb@126.com>
2020-03-11 11:06:07 +0800

    添加日志收集web程序模块

---
+++ app_logs_collect_web/src/main/java/com/bawei/app_logs_collect_web/AppLogsCollectWebApplication.java
@@ -0,0 +1,13 @@
+package com.bawei.app_logs_collect_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLogsCollectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLogsCollectWebApplication.class, args);
+	}
+
+}
---
+++ app_logs_collect_web/src/test/java/com/bawei/app_logs_collect_web/AppLogsCollectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bawei.app_logs_collect_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLogsCollectWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
